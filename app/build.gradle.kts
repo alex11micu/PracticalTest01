b12b29eab2bd4f2d6d9d20e3d73336d266c226b1
alex11micu <alexmicu271@gmail.com>
2024-11-12 18:55:18 +0200

    A2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ro.pub.cs.systems.eim.practicaltest01"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ro.pub.cs.systems.eim.practicaltest01"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/Colocviu1_MainActivity.kt
@@ -0,0 +1,11 @@
+package ro.pub.cs.systems.eim.practicaltest01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Colocviu1_MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_colocviu1_1_main)
+    }
+}
